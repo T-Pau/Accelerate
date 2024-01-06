1560d295aacc74d45b76465319802ac41116065a
Dieter Baron <dillo@nih.at>
2024-01-06 18:15:36 +0100

    Move explicitly used and pinned objects into library.

--- src/ObjectFile.cc
+++ src/ObjectFile.cc
@@ -179,6 +179,15 @@ void ObjectFile::add_object_file(const std::shared_ptr<ObjectFile>& file) {
     for (auto& library : file->imported_libraries) {
         import(library);
     }
+
+    explicitly_used_object_names.insert(file->explicitly_used_object_names.begin(), explicitly_used_object_names.end());
+    file->explicitly_used_object_names.clear();
+    explicitly_used_objects.insert(file->explicitly_used_objects.begin(), explicitly_used_objects.end());
+    file->explicitly_used_objects.clear();
+    for (const auto& pair: file->pinned_objects) {
+        pin(pair.first, pair.second.address);
+    }
+    file->pinned_objects.clear();
 }
 
 void ObjectFile::evaluate() {
